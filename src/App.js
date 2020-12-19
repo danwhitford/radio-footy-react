219ef983be1a49e5676c5e323dd22ae2bbc6e7e4
Daniel Whitford <whitford.dsw@gmail.com>
2020-12-19 15:00:52 +0000

    basic working

--- src/App.js
+++ src/App.js
@@ -4,21 +4,65 @@ import "./App.css";
 import FilterOpts from "./components/FilterOpts";
 
 function App() {
-  const [matches, setMatches] = useState([]);
+  const [matchDays, setMatchDays] = useState([]);
   const [filterCategory, setFilterCategory] = useState("None");
   const [filterValue, setFilterValue] = useState("None");
 
   useEffect(() => {
     fetch("http://localhost:8080/data.json")
       .then((r) => r.json())
-      .then((j) => setMatches(j));
-  }, [setMatches]);
+      .then((j) => setMatchDays(j));
+  }, [setMatchDays]);
 
   useEffect(() => {
-    if (filterCategory === "None") {
-      setFilterValue("None");
-    }
+    setFilterValue("None");
   }, [filterCategory, setFilterValue]);
+
+  const filterMatches = () => {
+    if (filterCategory === "None" || filterValue === "None") {
+      return matchDays;
+    }
+
+    if (filterCategory === "team") {
+      return matchDays
+        .map((day) => {
+          return {
+            ...day,
+            matches: day.matches.filter((match) =>
+              match.title.split(" v ").includes(filterValue)
+            ),
+          };
+        })
+        .filter((day) => day.matches.length > 0);
+    } else if (filterCategory === "station") {
+      return matchDays
+        .map((day) => {
+          return {
+            ...day,
+            matches: day.matches.filter(
+              (match) => match.station === filterValue
+            ),
+          };
+        })
+        .filter((day) => day.matches.length > 0);
+    } else if (filterCategory === "competition") {
+      return matchDays
+        .map((day) => {
+          return {
+            ...day,
+            matches: day.matches.filter(
+              (match) => match.competition === filterValue
+            ),
+          };
+        })
+        .filter((day) => day.matches.length > 0);
+    } else {
+      console.warn(
+        'Filter is not "None" but is not recognised: ' + filterCategory
+      );
+      return matchDays;
+    }
+  };
 
   return (
     <div className="App">
@@ -27,13 +71,13 @@ function App() {
         filterValue={filterValue}
         setFilterCategory={setFilterCategory}
         setFilterValue={setFilterValue}
-        matches={matches}
+        matches={matchDays}
       />
 
       <h1 className="title">Football</h1>
       <h2 className="subtitle">on the radio</h2>
 
-      {matches.map((day) => (
+      {filterMatches().map((day) => (
         <MatchDay key={day.date} date={day.date} matches={day.matches} />
       ))}
     </div>
--- src/components/FilterOpts.js
+++ src/components/FilterOpts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Select from "react-select";
 
 const FilterOpts = ({
   filterCategory,
@@ -9,15 +10,15 @@ const FilterOpts = ({
 }) => {
   let competitions = matches
     .flatMap((day) => day.matches.map((match) => match.competition))
-    .filter((x, i, a) => a.indexOf(x) == i)
+    .filter((x, i, a) => a.indexOf(x) === i)
     .sort();
   let teams = matches
     .flatMap((day) => day.matches.flatMap((match) => match.title.split(" v ")))
-    .filter((x, i, a) => a.indexOf(x) == i)
+    .filter((x, i, a) => a.indexOf(x) === i)
     .sort();
   let stations = matches
     .flatMap((day) => day.matches.map((match) => match.station))
-    .filter((x, i, a) => a.indexOf(x) == i)
+    .filter((x, i, a) => a.indexOf(x) === i)
     .sort();
 
   const filterOptions = () => {
@@ -37,29 +38,34 @@ const FilterOpts = ({
     }
   };
 
+  const filterCategoryOptions = [
+    { value: "team", label: "Team" },
+    { value: "station", label: "Station" },
+    { value: "competition", label: "Competition" },
+  ]
+
+  const filterValueOptions = filterOptions().map(opt => {return {
+    value: opt,
+    label: opt,
+  }})
+
   return (
     <div>
-      <span>Filter by:</span>
-      <select
-        value={filterCategory}
-        onChange={(e) => setFilterCategory(e.target.value)}
-      >
-        <option value="None">None</option>
-        <option value="team">Team</option>
-        <option value="station">Station</option>
-        <option value="competition">Competition</option>
-      </select>
+      <Select
+        placeholder="Filter by..."
+        onChange={(e) => {
+          setFilterCategory(e ? e.value : 'None')
+        }}
+        options={filterCategoryOptions}
+        value={filterCategoryOptions.find(o => o.value === filterCategory) || 'None'}
+        isClearable
+      />
       {filterCategory !== "None" && (
-        <select
-          disabled={filterCategory === "None"}
-          value={filterValue}
-          onChange={(e) => setFilterValue(e.target.value)}
-        >
-          <option value="None">None</option>
-          {filterOptions().map((opt) => (
-            <option value={opt}>{opt}</option>
-          ))}
-        </select>
+          <Select
+            options={filterValueOptions}
+            onChange={(e) => setFilterValue(e.value)}
+            value={filterValueOptions.find(o => o.value === filterValue) || 'None'}
+          />
       )}
     </div>
   );
